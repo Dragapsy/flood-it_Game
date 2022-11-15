34dab129bd92190c1be1fd42536c0fb2b0093908
dragapsy <elghaoutiayman20@gmail.com>
2022-11-15 15:28:01 +0100

    MAJ

---
+++ app/src/main/java/model/ArrayGrid.java
@@ -0,0 +1,44 @@
+package model;
+
+public class ArrayGrid implements Grid{
+
+    private Cell [][] cells;
+    private int numberOfRows;
+    private int numberOfColumns;
+
+
+    public ArrayGrid(int numberOfRows,int numberOfColumns){
+        this.numberOfColumns=numberOfColumns;
+        this.numberOfRows=numberOfRows;
+        if(numberOfRows==0 | numberOfRows<0 | numberOfColumns==0 | numberOfRows<0 | numberOfColumns<0)  {
+            throw new IllegalArgumentException("numberOfRows or numberOfColumns can't be equal to zero or a negative value");
+        }
+        else {
+            this.cells=new Cell[numberOfRows][numberOfColumns];
+            Cell cell=new SquareCell();
+            for (int i=0; i<numberOfRows;i++){
+                for(int j=0; j<numberOfColumns;j++){
+                    cells[i][j]=cell;
+                }
+            }
+
+        }
+    }
+
+
+
+    @Override
+    public Cell getCell(int row, int column) {
+        return null;
+    }
+
+    @Override
+    public int getNumberOfRows() {
+        return 0;
+    }
+
+    @Override
+    public int getNumberOfColumns() {
+        return 0;
+    }
+}
