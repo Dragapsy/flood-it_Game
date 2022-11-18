b771349fa91a0ac314a1f584a8ae366d495c9ef2
dragapsy <elghaoutiayman20@gmail.com>
2022-11-18 04:09:47 +0100

    Tache 4 (La methode nextColor de la classe CyclicColorGenerator).

--- app/src/main/java/model/CyclicColorGenerator.java
+++ app/src/main/java/model/CyclicColorGenerator.java
@@ -7,12 +7,19 @@ import java.util.List;
 public class CyclicColorGenerator implements ColorGenerator{
 
     private List<Color> colors;
+    private int counter=1;
 
     public CyclicColorGenerator(List<Color> colors){
         this.colors = colors;
     }
     @Override
     public Color nextColor(Cell cell) {
-        return null;
+        if(counter >= this.colors.size()){
+            counter=0;
+        }
+
+        Color newColor = this.colors.get(counter);
+        counter++;
+        return newColor;
     }
 }
